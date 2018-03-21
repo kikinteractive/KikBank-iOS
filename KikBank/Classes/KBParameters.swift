65e0e4f3a846b9d365fa2c6b9fec05bbe47444e6
Yucheng Yan <yanyucheng007@hotmail.com>
2018-03-21 13:03:45 -0400

    modify comment

--- KikBank/Classes/KBParameters.swift
+++ KikBank/Classes/KBParameters.swift
@@ -30,7 +30,7 @@ import Foundation
 
 /// Specify how data should be fetched and saved
 @objc public class KBParameters: NSObject {
-    /// The interval which the cached data is considered valid
+    /// The date after which the cached data is considered invalid
     @objc public var expiryDate: Date?
 
     /// The data read type
